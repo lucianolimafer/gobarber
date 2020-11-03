42f0d76bcb2f8cf3f3f100e78c1aabcba0b173cd
lucianolimafer <lucianolimaf3r@gmail.com>
2020-11-03 20:39:59 -0300

    :rocket: Feature: add password column

--- backend/src/database/migrations/1604444230610-CreateUsers.ts
+++ backend/src/database/migrations/1604444230610-CreateUsers.ts
@@ -24,6 +24,10 @@ export default class CreateUsers1604444230610 implements MigrationInterface {
             isUnique: true,
           },
           {
+            name: 'password',
+            type: 'varchar',
+          },
+          {
             name: 'created_at',
             type: 'timestamp',
             default: 'now()',
