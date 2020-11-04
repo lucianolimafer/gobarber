c591216311fa9dc6330ff6ff95619b7a798534d9
lucianolimafer <lucianolimaf3r@gmail.com>
2020-11-03 21:21:45 -0300

    :art: Add relational column manyToOne

--- backend/src/models/Appointment.ts
+++ backend/src/models/Appointment.ts
@@ -1,5 +1,14 @@
-import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn
+} from 'typeorm';
 
+import User from './User';
 @Entity('appointments')
 class Appointment {
   @PrimaryGeneratedColumn('uuid')
@@ -7,6 +16,10 @@ class Appointment {
 
   @Column()
   provider_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'provider_id'})
+  provider: User;
 
   @Column('time with time zone')
   date: Date;
