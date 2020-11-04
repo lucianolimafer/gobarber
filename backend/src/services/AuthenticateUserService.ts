fab451cba6473d57133090dc9de2e8ce03848bff
lucianolimafer <lucianolimaf3r@gmail.com>
2020-11-04 17:15:35 -0300

    :lock: Feat: generate the token

--- backend/src/services/AuthenticateUserService.ts
+++ backend/src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -11,6 +12,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -31,7 +33,12 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password.');
     }
 
-    return { user }
+    const token = sign({}, '7a2d023d0e5792f5fadb707a9d314c72', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token }
   }
 }
 
