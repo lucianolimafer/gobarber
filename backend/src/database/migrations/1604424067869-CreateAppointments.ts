d47284ef323a05a05a6a6d30484c821824f4df1b
lucianolimafer <lucianolimaf3r@gmail.com>
2020-11-03 21:10:08 -0300

    :art: Change the type id

--- backend/src/database/migrations/1604424067869-CreateAppointments.ts
+++ backend/src/database/migrations/1604424067869-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1604424067869 implements MigrationInterfa
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()'
