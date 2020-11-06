6f0623bce7ce8882c0bcdd28ec94881d79779ee2
lucianolimafer <lucianolimaf3r@gmail.com>
2020-11-06 14:43:24 -0300

    :art: Add erros status global

--- backend/src/services/AuthenticateUserService.ts
+++ backend/src/services/AuthenticateUserService.ts
@@ -1,8 +1,9 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
-import authConfig from '../config/auth';
 
+import authConfig from '../config/auth';
+import AppError from '../errors/AppError';
 
 import User from '../models/User';
 
@@ -26,13 +27,13 @@ class AuthenticateUserService {
     });
 
     if (!user) {
-      throw new Error('Incorrect email/password.')
+      throw new AppError('Incorrect email/password.', 401)
     }
 
     const passwordMatched = await compare(password, user.password);
 
     if(!passwordMatched) {
-      throw new Error('Incorrect email/password.');
+      throw new AppError('Incorrect email/password.', 401);
     }
 
     const { secret, expiresIn } = authConfig.jwt;
--- backend/src/services/UpdateUserAvatarService.ts
+++ backend/src/services/UpdateUserAvatarService.ts
@@ -4,6 +4,8 @@ import fs from 'fs';
 import User from '../models/User';
 import uploadConfig from '../config/upload';
 
+import AppError from '../errors/AppError';
+
 interface Request {
   user_id: string;
   avatarFilename: string;
@@ -16,7 +18,7 @@ class UpdateUserAvatarService{
     const user = await usersRepository.findOne(user_id);
 
     if(!user){
-      throw new Error('Only autheticated user can change the avatar.')
+      throw new AppError('Only autheticated user can change the avatar.', 401);
     }
 
     if(user.avatar) {
