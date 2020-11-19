b4e3bc6081f2d1e9da24bd883a77e71ad1dc08ce
lucianolimafer <lucianolimaf3r@gmail.com>
2020-11-19 19:42:20 -0300

    :art: Import external fonts

--- mobile/src/pages/SignIn/styles.ts
+++ mobile/src/pages/SignIn/styles.ts
@@ -1,5 +1,14 @@
 import styled from 'styled-components/native';
 
-const Container = styled.View``;
+export const Container = styled.View`
+  flex: 100%;
+  justify-content: center;
+  align-items: center;
+`;
 
-export {Container};
+export const Title = styled.Text`
+  font-size: 24px;
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Medium';
+  margin: 54px 0 24px;
+`;
