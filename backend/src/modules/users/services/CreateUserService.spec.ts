a52eed1595cfe9a521d6bae623334d348295200e
lucianolimafer <lucianolimaf3r@gmail.com>
2020-12-08 15:58:08 -0300

    :white_check_mark: add createUser test AppError

--- backend/src/modules/users/services/CreateUserService.spec.ts
+++ backend/src/modules/users/services/CreateUserService.spec.ts
@@ -19,4 +19,24 @@ describe('CreateUser', () => {
 
     expect(user).toHaveProperty('id');
   });
+
+  it('should be able to create a new user', async () => {
+    const fakeUserssRepository = new FakeUsersRepository();
+    const createUser = new CreateUserService(
+      fakeUserssRepository,
+    );
+
+    await createUser.execute({
+      name: 'Luciano Lima',
+      email: 'luciano@gmail.com',
+      password: '16545641',
+    });
+
+    expect(createUser.execute({
+      name: 'Luciano Lima',
+      email: 'luciano@gmail.com',
+      password: '16545641',
+    })
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
