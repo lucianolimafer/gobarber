6e9626e826195df962373936f26bb81ab720953d
lucianolimafer <lucianolimaf3r@gmail.com>
2020-12-08 15:50:30 -0300

    :white_check_mark: add createUser test

---
+++ backend/src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,22 @@
+import AppError from '@shared/errors/AppError';
+import 'reflect-metadata';
+
+import FakeUsersRepository from '../../users/repositories/Fakes/FakeUsersRepository';
+import CreateUserService from './CreateUserService';
+
+describe('CreateUser', () => {
+  it('should be able to create a new user', async () => {
+    const fakeUserssRepository = new FakeUsersRepository();
+    const createUser = new CreateUserService(
+      fakeUserssRepository,
+    );
+
+    const user = await createUser.execute({
+      name: 'Luciano Lima',
+      email: '51515354',
+      password: '16545641',
+    });
+
+    expect(user).toHaveProperty('id');
+  });
+});
