cdfab3447910ddb9ade3a8c2cf3ff9b2bba3077e
lucianolimafer <lucianolimaf3r@gmail.com>
2020-12-17 15:41:52 -0300

    :rocket: send toke into email send

--- backend/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ backend/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -34,9 +34,9 @@ class SendForgotPasswordEmailService {
       throw new AppError('User does not exists.');
     }
 
-    await this.userTokensRepository.generate(user.id);
+    const {token} = await this.userTokensRepository.generate(user.id);
 
-    this.mailProvider.sendMail(email, 'Pedido de recuperação de senha')
+    await this.mailProvider.sendMail(email, `Pedido de recuperação de senha ${token}`)
   }
 }
 
