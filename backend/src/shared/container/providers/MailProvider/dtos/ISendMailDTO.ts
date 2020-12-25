394509afe0f5d3bc10775b041a50d39e43de8b98
lucianolimafer <lucianolimaf3r@gmail.com>
2020-12-25 17:56:26 -0300

    :rocket: Add feat email templates

---
+++ backend/src/shared/container/providers/MailProvider/dtos/ISendMailDTO.ts
@@ -0,0 +1,13 @@
+import IParseMailTemplateDTO from '@shared/container/providers/MailTemplateProvider/dtos/IParseMailTemplateDTO';
+
+interface IMailContact {
+  name: string;
+  email: string;
+}
+
+export default interface ISendMailDTO {
+  to: IMailContact;
+  from?:IMailContact;
+  sbject: string;
+  templateData: IParseMailTemplateDTO;
+}
