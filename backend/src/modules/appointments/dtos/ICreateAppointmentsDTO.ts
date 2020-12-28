62fb0edd4b656c7d1deb4096e043c4ccb2eb92bf
lucianolimafer <lucianolimaf3r@gmail.com>
2020-12-28 14:34:47 -0300

    :art: fix changes

--- backend/src/modules/appointments/dtos/ICreateAppointmentsDTO.ts
+++ backend/src/modules/appointments/dtos/ICreateAppointmentsDTO.ts
@@ -1,5 +1,4 @@
 export default interface ICreateAppointmentsDTO {
   provider_id: string;
-  user_id: string;
   date: Date;
 }
--- backend/src/modules/appointments/services/CreateAppointmentService.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,4 +1,4 @@
-import { getHours, isBefore, startOfHour, format } from 'date-fns';
+import { startOfHour } from 'date-fns';
 import { inject, injectable } from 'tsyringe';
 
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
@@ -10,7 +10,6 @@ import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 interface IRequest {
   provider_id: string;
   date: Date,
-  user_id: string;
 }
 
 @injectable()
@@ -20,21 +19,9 @@ class CreateAppointmentService {
     private appointmentsRepository: IAppointmentsRepository
   ) {}
 
-  public async execute({ date, provider_id, user_id }: IRequest): Promise<Appointment> {
+  public async execute({ date, provider_id }: IRequest): Promise<Appointment> {
 
     const appointmentDate = startOfHour(date);
-
-    if(isBefore(appointmentDate, Date.now())) {
-      throw new AppError("You can't create an appointment on a past date.")
-    }
-
-    if(user_id === provider_id) {
-      throw new AppError("You can't create an appointment with yourself.")
-    }
-
-    if(getHours(appointmentDate) < 8 || getHours(appointmentDate) > 20) {
-      throw new AppError("You can only create appointments between 8am and 8pm")
-    }
 
     const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(appointmentDate);
 
@@ -45,10 +32,8 @@ class CreateAppointmentService {
     const appointment = await this.appointmentsRepository.create({
       provider_id,
       date: appointmentDate,
-      user_id
-    });
 
-    const dateFormated = format(appointmentDate, "dd/MM/yyyy 'às' HH:mm'h'");
+    });
 
     return appointment;
   }
