9a11ec73f6c1fedc789b06f2af46aaf119b55398
lucianolimafer <lucianolimaf3r@gmail.com>
2021-01-01 14:14:25 -0300

    :rocket: add method to find all providers per MM

--- backend/src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ backend/src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,7 +1,9 @@
 import ICreateAppointmentsDTO from '../dtos/ICreateAppointmentsDTO';
+import IFindAllInMonthFromProviderDTO from '../dtos/IFindAllInMonthFromProviderDTO';
 import Appointment from '../infra/typeorm/entities/Appointment';
 
 export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentsDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
+  findAllInMonthFromProvider(data: IFindAllInMonthFromProviderDTO): Promise<Appointment[]>;
 }
--- backend/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ backend/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,9 +1,10 @@
 import { v4 as uuidv4 } from 'uuid';
-import { isEqual } from 'date-fns';
+import { isEqual, getMonth, getDate, getYear } from 'date-fns';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import Appointment from '../../infra/typeorm/entities/Appointment';
 import ICreateAppointmentsDTO from '@modules/appointments/dtos/ICreateAppointmentsDTO';
+import IFindAllInMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO';
 
 class AppointmentsRepository implements IAppointmentsRepository {
   private appointments: Appointment[] = [];
@@ -16,12 +17,24 @@ class AppointmentsRepository implements IAppointmentsRepository {
     return findAppointment;
   }
 
+  public async findAllInMonthFromProvider({
+    provider_id,
+    month,
+    year
+  }: IFindAllInMonthFromProviderDTO): Promise<Appointment[]> {
+    const appointments = this.appointments.filter(appointment => {
+      return (
+        appointment.provider_id === provider_id &&
+        getMonth(appointment.date) + 1 === month &&
+        getYear(appointment.date) === year
+      );
+    });
+
+    return appointments;
+  }
+
   public async create({ provider_id, date }: ICreateAppointmentsDTO): Promise<Appointment> {
     const appointment = new Appointment();
-
-    //appointment.id = uuid();
-    //appointment.date = date;
-    //appointment.provider_id = provider_id;
 
     Object.assign(appointment, {
       id: uuidv4(),
