56d0f248e74766198d7c8580debf7b0d5581be20
lucianolimafer <lucianolimaf3r@gmail.com>
2021-01-05 15:52:52 -0300

    :bug: Converting date

--- backend/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ backend/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { parseISO } from 'date-fns';
 import { container } from 'tsyringe';
 
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
@@ -9,14 +10,16 @@ export default class AppointmentsController {
     const user_id = request.user.id;
     const { provider_id, date } = request.body;
 
+    const parseDate = parseISO(date);
+
     const createAppointment = container.resolve(CreateAppointmentService);
 
     const appointment = await createAppointment.execute({
-      date:
-        date,
-        provider_id,
-        user_id,
-      });
+
+      date: parseDate,
+      provider_id,
+      user_id,
+    });
 
     return response.json(appointment);
   }
