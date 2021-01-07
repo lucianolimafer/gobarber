a32065a6f9dcfbdc8523817cc777dfb69602ecce
lucianolimafer <lucianolimaf3r@gmail.com>
2021-01-07 12:29:52 -0300

    :white_check_mark: Add test to implementation from appointments index per provider

---
+++ backend/src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,46 @@
+
+
+import AppError from '@shared/errors/AppError';
+import 'reflect-metadata';
+
+import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+import ListProviderAppointmentsService from './ListProviderAppointmentsService';
+
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProviderAppointments: ListProviderAppointmentsService;
+
+
+
+describe('ListProviderAppointments', () => {
+
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderAppointments = new ListProviderAppointmentsService(fakeAppointmentsRepository);
+  });
+
+
+  it('should be able to list the appointments from specific day', async () => {
+    const appointment1 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider',
+      user_id: 'user',
+      date: new Date(2021, 1, 20, 8, 0, 0),
+    });
+
+    const appointment2 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider',
+      user_id: 'user',
+      date: new Date(2021, 1, 20, 9, 0, 0),
+    });
+
+    const appointments = await listProviderAppointments.execute({
+      provider_id: 'provider',
+      year: 2021,
+      month: 2,
+      day: 20,
+    });
+
+    await expect(appointments).toEqual([appointment1, appointment2]);
+
+  });
+})
