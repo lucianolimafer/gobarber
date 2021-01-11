1dfbbceb049b305b93fe19e1b1ba2fa9bbf60575
lucianolimafer <lucianolimaf3r@gmail.com>
2021-01-11 18:59:35 -0300

    :rocket: + :lock: Add data validation on routes from the app

--- backend/src/modules/appointments/infra/http/routes/providers.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
+
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProvidersController from '../controllers/ProvidersController';
@@ -13,8 +15,16 @@ const providerMonthController = new ProviderMonthAvailabilityController();
 
 providersRouter.use(ensureAuthenticated);
 providersRouter.get('/', providersController.index);
-providersRouter.get('/:provider_id/month-availability', providerMonthController.index);
-providersRouter.get('/:provider_id/day-availability', providerDayController.index);
+providersRouter.get('/:provider_id/month-availability', celebrate({
+  [Segments.PARAMS]: {
+    provider_id: Joi.string().uuid().required(),
+  }
+}), providerMonthController.index);
+providersRouter.get('/:provider_id/day-availability', celebrate({
+  [Segments.PARAMS]: {
+    provider_id: Joi.string().uuid().required(),
+  }
+}), providerDayController.index);
 
 
 export default providersRouter;
--- backend/src/modules/users/infra/http/routes/users.routes.ts
+++ backend/src/modules/users/infra/http/routes/users.routes.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
+
 import multer from 'multer';
 import uploadConfig from '@config/upload';
 
@@ -15,7 +17,13 @@ const userAvatarController = new UserAvatarController();
 
 const upload = multer(uploadConfig);
 
-usersRouter.post('/', usersController.create)
+usersRouter.post('/', celebrate({
+  [Segments.BODY]: {
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  }
+}), usersController.create)
 usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'),  userAvatarController.update);
 
 export default usersRouter;
