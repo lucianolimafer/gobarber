ea58d4a7806d3ba5c355442666eb839b4b043084
lucianolimafer <lucianolimaf3r@gmail.com>
2021-01-15 18:44:31 -0300

    :art: Change request from body to query

--- backend/src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
+++ backend/src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
@@ -7,15 +7,15 @@ import ListProviderDayAvailabilityService from '@modules/appointments/services/L
 export default class ProviderDayAvailabilityController {
   public async index(request: Request, response: Response): Promise<Response> {
     const {provider_id} = request.params;
-    const { month, year, day } = request.body;
+    const { month, year, day } = request.query;
 
     const listProviderProviderDayAvailability = container.resolve(ListProviderDayAvailabilityService);
 
     const availability = await listProviderProviderDayAvailability.execute({
       provider_id,
-      month,
-      year,
-      day
+      month: Number(month),
+      year: Number(year),
+      day: Number(day)
     })
 
 
