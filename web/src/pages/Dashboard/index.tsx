14e9ea7616c635315281e4feed20341a002bb74f
lucianolimafer <lucianolimaf3r@gmail.com>
2021-01-16 15:12:26 -0300

    :art: :rocket: Create the list of appointments

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { FiClock, FiPower } from 'react-icons/fi';
 import {
@@ -9,6 +9,8 @@ import {
   Content,
   Schedule,
   NextAppointment,
+  Section,
+  Appointment,
   Calendar,
 } from './styles';
 
@@ -16,6 +18,8 @@ import logoImg from '../../assets/seuluisLogo.png';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   const { signOut, user } = useAuth();
 
   return (
@@ -61,6 +65,54 @@ const Dashboard: React.FC = () => {
               </span>
             </div>
           </NextAppointment>
+          <Section>
+            <strong>Manhã</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                10:00
+              </span>
+              <div>
+                <img
+                  src="https://i1.rgstatic.net/ii/profile.image/908178761981954-1593538098349_Q512/Thiago_Moraes26.jpg"
+                  alt="Avatar"
+                />
+
+                <strong>Thiago Moraes</strong>
+              </div>
+            </Appointment>
+            <Appointment>
+              <span>
+                <FiClock />
+                10:00
+              </span>
+              <div>
+                <img
+                  src="https://i1.rgstatic.net/ii/profile.image/908178761981954-1593538098349_Q512/Thiago_Moraes26.jpg"
+                  alt="Avatar"
+                />
+
+                <strong>Thiago Moraes</strong>
+              </div>
+            </Appointment>
+          </Section>
+          <Section>
+            <strong>Tarde e Noite</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                10:00
+              </span>
+              <div>
+                <img
+                  src="https://i1.rgstatic.net/ii/profile.image/908178761981954-1593538098349_Q512/Thiago_Moraes26.jpg"
+                  alt="Avatar"
+                />
+
+                <strong>Thiago Moraes</strong>
+              </div>
+            </Appointment>
+          </Section>
         </Schedule>
         <Calendar />
       </Content>
