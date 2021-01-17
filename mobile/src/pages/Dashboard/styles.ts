81c052baaedead5e946974a26e69be205f08cc5b
lucianolimafer <lucianolimaf3r@gmail.com>
2021-01-17 20:56:40 -0300

    :rocket: Search and get providers form API

--- mobile/src/pages/Dashboard/styles.ts
+++ mobile/src/pages/Dashboard/styles.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components/native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { FlatList } from 'react-native';
+import { Provider } from './index';
 
 
 export const Container = styled.View`
@@ -37,4 +39,8 @@ export const UserAvatar = styled.Image`
   width: 56px;
   height: 56px;
   border-radius: 28px;
+`;
+
+export const ProvidersList = styled(FlatList as new () => FlatList<Provider>)`
+
 `;
